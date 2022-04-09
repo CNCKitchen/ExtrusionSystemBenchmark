12258fed7af3ff6eedf4437985ebd67ceca33085
Guikai <guilbert04@gmail.com>
2022-04-08 22:28:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 
 
-
 data.addEventListener("submit", function (e) {
-
-    //Varibles
+    e.preventDefault();
     let data = document.querySelector('#data');
     let bedWidth = Number(document.querySelector('#bedWidth').value);
     let bedLength = Number(document.querySelector('#bedLength').value);
@@ -21,7 +19,7 @@ data.addEventListener("submit", function (e) {
     let retractionSpeed = Number(document.querySelector('#retractionSpeed').value);
     let blobHeight = Number(document.querySelector('#blobHeight').value);
     let extrusionAmount = Number(document.querySelector('#extrusionAmount').value);
-    let direction = Number(document.querySelector('#direction').value);
+    let direction = document.querySelector('#direction');
     let xSpacing = Number(document.querySelector('#temperatureSpacing').value);
     let ySpacing = Number(document.querySelector('#flowSpacing').value);
     let startFlow = Number(document.querySelector('#startFlow').value);
@@ -36,11 +34,8 @@ data.addEventListener("submit", function (e) {
     let code = document.querySelector('#code');
     let resultado = [];
     let slot = 0;
+    //Varibles
 
-
-
-
-    e.preventDefault();
     if (tempSteps == 1) {
 
         tempSteps = Math.ceil(flowSteps / Math.floor((bedLength - 2 * bedMargin) / ySpacing));
@@ -48,7 +43,7 @@ data.addEventListener("submit", function (e) {
         tempOffset = 0;
     }
 
-    if (direction.value == 1) {
+    if (direction.checked == true) {
 
         bedLength = 0
         bedMargin = bedMargin * -1
@@ -57,20 +52,12 @@ data.addEventListener("submit", function (e) {
 
 
     for (let i = 1; i <= tempSteps; i++) {
-        console.log(i, tempSteps);
+
 
         if (tempOffset == 0) {
 
             startFlow = startFlow + flowSteps * flowOffset;
-            console.log(startFlow);
-
         }
-
-        console.log(` --------####### ${startTemp + (i - 1) * tempOffset} "C"
-                      G4 S0 ; Dwell
-    
-                  M109 R${startTemp + (i - 1) * tempOffset}`
-        );
 
         slot = slot + 1
         resultado[slot] =
@@ -93,36 +80,11 @@ data.addEventListener("submit", function (e) {
 
             extrusionSpeed = Math.round(blobHeight / (extrusionAmount / ((startFlow + (j - 1) * flowOffset) / (Math.atan(1) * filamentDiameter * filamentDiameter) * 60)), 2)
 
-
-
-            console.log(`
-            
-            
-            ####### ${startFlow + (j - 1) * flowOffset} mm3/s
-                        M117 ${startTemp + (i - 1) * tempOffset} °C // " ${startFlow + (j - 1) * flowOffset} mm3/s
-
-                        G0 X ${Math.abs(bedMargin) + ((i - 1) * (primeLength + wipeLength + xSpacing))} Y${(bedLength - bedMargin) - (j - 1) * ySpacing} Z${0.5 + blobHeight + 5} F${movementSpeed * 60}
-                                
-                        G4 S${stabilizationTime}; Stabalize
-                        G0 Z0.3; Drop down")
-                        G1 X${Math.abs(bedMargin) + primeLength + ((i - 1) * (primeLength + wipeLength + xSpacing))} E${primeAmount} F${(primeSpeed * 60)} ;Prime
-                        G1 E${-1 * retractionDistance} F${retractionSpeed * 60}; Retract
-                        G0 X${Math.abs(bedMargin) + primeLength + wipeLength + ((i - 1) * (primeLength + wipeLength + xSpacing))} F${movementSpeed * 60}; Wipe
-                        G0 Z0.5; Lift")
-                        G1 E${retractionDistance} F${retractionSpeed * 60}; De - Retract
-                                
-
-                        G1 Z"${0.5 + blobHeight} E${extrusionAmount} F${extrusionSpeed}; Extrude
-                        G1 E${-1 * retractionDistance} F${retractionSpeed * 60}; Retract
-                        G0 Z${0.5 + blobHeight + 5}; Lift
-                        G0 X${Math.abs(bedMargin) + ((i - 1) * (primeLength + wipeLength + xSpacing))} Y${(bedLength - bedMargin) - (j - 1) * ySpacing} F${movementSpeed * 60}
-                        G92 E0; Reset Extruder")
-                        `);
             slot = slot + 1;
             resultado[slot] =
                 `;---------------------------####### ${startFlow + (j - 1) * flowOffset} mm3/s
-            M117 ${startTemp + (i - 1) * tempOffset} °C // " ${startFlow + (j - 1) * flowOffset} mm3/s
-            G0 X ${Math.abs(bedMargin) + ((i - 1) * (primeLength + wipeLength + xSpacing))} Y${(bedLength - bedMargin) - (j - 1) * ySpacing} Z${0.5 + blobHeight + 5} F${movementSpeed * 60}
+            M117 ; ${startTemp + (i - 1) * tempOffset} °C // " ${startFlow + (j - 1) * flowOffset} mm3/s
+            G0 X${Math.abs(bedMargin) + ((i - 1) * (primeLength + wipeLength + xSpacing))} Y${(bedLength - bedMargin) - (j - 1) * ySpacing} Z${0.5 + blobHeight + 5} F${movementSpeed * 60}
             G4 S${stabilizationTime}; Stabalize
             G0 Z0.3; Drop down")
             G1 X${Math.abs(bedMargin) + primeLength + ((i - 1) * (primeLength + wipeLength + xSpacing))} E${primeAmount} F${(primeSpeed * 60)} ;Prime
@@ -130,22 +92,19 @@ data.addEventListener("submit", function (e) {
             G0 X${Math.abs(bedMargin) + primeLength + wipeLength + ((i - 1) * (primeLength + wipeLength + xSpacing))} F${movementSpeed * 60}; Wipe
             G0 Z0.5; Lift")
             G1 E${retractionDistance} F${retractionSpeed * 60}; De - Retract
-            G1 Z"${0.5 + blobHeight} E${extrusionAmount} F${extrusionSpeed}; Extrude
+            G1 Z${0.5 + blobHeight} E${extrusionAmount} F${extrusionSpeed}; Extrude
             G1 E${-1 * retractionDistance} F${retractionSpeed * 60}; Retract
             G0 Z${0.5 + blobHeight + 5}; Lift
             G0 X${Math.abs(bedMargin) + ((i - 1) * (primeLength + wipeLength + xSpacing))} Y${(bedLength - bedMargin) - (j - 1) * ySpacing} F${movementSpeed * 60}
             G92 E0; Reset Extruder")
-
             `
-
-
         }
     }
 
     code.innerText =
         ` *** CNC Kitchen Auto Flow Pattern Generator 0.93"
 *** 02/04/26 Stefan Hermann"
-""
+
 Generation Setting
 ####### Settings
 ${comment.value}
@@ -186,7 +145,12 @@ M190 S ${bedTemp}; Set Bed Temperature & Wait
 M106 S${Math.round(fanSpeed * 255 / 100, 0)} ; Set Fan Speed
 
 ${resultado}
-
 `
+})
+
+data.addEventListener("reset", function (e) {
+
+    document.getElementById('data').reset();
+    code.innerText = `Here will be the gcode.`
 
 })
