d4f334cdd0bafb5f0200b3810df4ce737d5863a5
Guikai <guilbert04@gmail.com>
2022-04-10 21:35:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -145,6 +145,14 @@ M190 S ${bedTemp}; Set Bed Temperature & Wait
 M106 S${Math.round(fanSpeed * 255 / 100, 0)} ; Set Fan Speed
 
 ${resultado}
+
+M104 S0
+M140 S0
+;Retract the filament
+G92 E1
+G1 E-1 F300
+G28 X0 Y0
+M84
 `
 })
 
